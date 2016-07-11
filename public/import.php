6b754e503382a64d0bbef7631b92a15a07f9d9eb
phoulin <phou.lin1@gmail.com>
2016-07-11 21:24:34 +0700

    test sny

--- public/import.php
+++ public/import.php
@@ -10,7 +10,7 @@ $dbName = 'ppbakery';
 $dir = dirname(__FILE__)."/db_backup/";
 //fopen($dir."db-backup-".date('Y-m-d').".sql", 'w') or die("Can't create file");
 
-$source = ("http://192.168.0.122/PPbakery/public/db_backup/1-db-backup-".date('Y-m-d').".sql");
+$source = ("http://192.168.0.131/PPbakery/public/db_backup/1-db-backup-".date('Y-m-d').".sql");
 
 //$source = ("http://192.168.0.10/db_backup/text.sql");
 $saveFile = $dir."1-db-backup-".date('Y-m-d').".sql";
