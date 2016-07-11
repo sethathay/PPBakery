edfcef37897953e0517e1512a9e4aa78fb1c68b2
phoulin <phou.lin1@gmail.com>
2016-07-11 21:34:36 +0700

    test syn

--- public/import.php
+++ public/import.php
@@ -7,13 +7,14 @@ $dbUser = 'root';
 $dbPass = '';
 $dbName = 'ppbakery';
 
-$dir = dirname(__FILE__)."/db_backup/";
+$dir = dirname(__FILE__)."\\db_backup\\";
 //fopen($dir."db-backup-".date('Y-m-d').".sql", 'w') or die("Can't create file");
 
 $source = ("http://192.168.0.131/PPbakery/public/db_backup/1-db-backup-".date('Y-m-d').".sql");
 
 //$source = ("http://192.168.0.10/db_backup/text.sql");
 $saveFile = $dir."1-db-backup-".date('Y-m-d').".sql";
+
 $data = file_get_contents($source);
 
 $handle = fopen($saveFile,"w");
@@ -71,7 +72,6 @@ while( $deadline>time() AND ($line=fgets($fp, 1024000)) ){
         $queryCount++;
     }
 }
-
 unlink($saveFile);
 
 ?>
