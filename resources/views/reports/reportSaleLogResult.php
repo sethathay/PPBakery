6d9a2eb1dab665033a560d5670dfd5afc865468a
phoulin <phou.lin1@gmail.com>
2016-07-26 23:22:57 +0700

    update report

--- resources/views/reports/reportSaleLogResult.php
+++ resources/views/reports/reportSaleLogResult.php
@@ -13,8 +13,15 @@
     </thead>
     <tbody>
     	<?php if(count($userSaleLog) > 0){?>
+			<?php $total_kh = $total_us = $sy_total = $balances = 0; ?>
 			<?php foreach($userSaleLog as $detail){?>
 			<?php $balance = ($detail->total_kh+($detail->total_us*$exchangerate->riel))-$detail->sy_total;?>
+			<?php
+				$total_kh = $detail->total_kh + $total_kh;
+				$total_us = $detail->total_us + $total_us;
+				$sy_total = $detail->sy_total + $sy_total;
+				$balances  = $balance + $balances;
+			?>
         	<tr>
 				<td><?php echo $detail->u_name; ?></td>
 				<td style="text-align:center"><?php echo $detail->dates; ?></td>
@@ -26,9 +33,16 @@
 				<td style="text-align:right; color:<?php echo ($balance>0)?'blue':'red';?>"><?php echo number_format($balance); ?></td>
 			</tr>
 			<?php }?>
+			<tr>
+				<td style="text-align:right; font-weight:bold;" colspan="4">សរុប</td>
+				<td style="text-align:right; font-weight:bold;"><?php echo number_format($total_kh);?></td>
+				<td style="text-align:right; font-weight:bold;"><?php echo number_format($total_us);?></td>
+				<td style="text-align:right; font-weight:bold;"><?php echo number_format($sy_total);?></td>
+				<td style="text-align:right; font-weight:bold;"><?php echo number_format($balances);?></td>
+			</tr>
         <?php }else{?>
         
-        <tr class="empty_data"><td colspan="7" style="text-align:center;">គ្នានទិន្ន័យនៅក្នុងតារាងទេ</td></tr>		
+        <tr class="empty_data"><td colspan="8" style="text-align:center;">គ្នានទិន្ន័យនៅក្នុងតារាងទេ</td></tr>		
         
         <?php }?>
     </tbody>
